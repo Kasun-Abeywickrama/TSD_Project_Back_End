629c0570ba09cf2067992ee117196d2bd8c9686c
HANSA SITHURUWAN <sithuruwan2002@gmail.com>
2024-01-12 15:07:08 +0530

    Update urls.py

--- tsd_project/tsd_main_app/urls.py
+++ tsd_project/tsd_main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .web_app_views import LogoutView, PageListCreateView, PageRetrieveUpdateDeleteView, RegisterView, RoleListCreateView, SigninView, QuestionCreatingView, QuestionSendingView, QuestionUpdatingView, QuestionDeleteView, QuestionSelectingView
+from .web_app_views import LogoutView, PageListCreateView, PageRetrieveUpdateDeleteView, RegisterView, ResultsListCreateView, RoleListCreateView, SetAppointment, SigninView, QuestionCreatingView, QuestionSendingView, QuestionUpdatingView, QuestionDeleteView, QuestionSelectingView,ResultsRetrieveUpdateDeleteView
 
 
 urlpatterns = [
+    path('set-appointment/<int:pk>/', SetAppointment.as_view(), name='set-appointment-detail'),
+    path('quiz-result/<int:pk>/', ResultsRetrieveUpdateDeleteView.as_view(), name='result-retrieve-update-delete'),
+    path('result-list/', ResultsListCreateView.as_view(), name='result-list'),
     path('role/', RoleListCreateView.as_view(), name='role-list-create'),
     path('logout/', LogoutView.as_view(), name ='logout'),
     path('register/', RegisterView.as_view(), name='register'),
@@ -15,7 +18,6 @@ urlpatterns = [
     path('update_question/', QuestionUpdatingView.as_view(), name='update-question'),
     path('delete_question/', QuestionDeleteView.as_view(), name='delete-question'),
     path('select_question/', QuestionSelectingView.as_view(), name='select_question'),
-
 ]
 
 
