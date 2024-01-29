2731aa7c8f23346e958ed40a1792425765aa8a27
HANSA SITHURUWAN <sithuruwan2002@gmail.com>
2024-01-29 21:34:19 +0530

    Update settings.py

--- tsd_project/tsd_project/settings.py
+++ tsd_project/tsd_project/settings.py
@@ -135,18 +135,18 @@ WSGI_APPLICATION = 'tsd_project.wsgi.application'
 
 DATABASES = {
     'default': {
-         #'ENGINE': 'django.db.backends.mysql',
-         #'NAME': 'MindCare$tsd_project',
-         #'USER': 'MindCare',
-         #'PASSWORD': 'MySQL2024$',
-         #'HOST': 'MindCare.mysql.pythonanywhere-services.com',
-         #'PORT': '3306',
          'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'tsdproject',
-         'USER': 'root',
-         'PASSWORD': '20021028',
-         'HOST': 'localhost',
+         'NAME': 'MindCare$tsd_project',
+         'USER': 'MindCare',
+         'PASSWORD': 'MySQL2024$',
+         'HOST': 'MindCare.mysql.pythonanywhere-services.com',
          'PORT': '3306',
+         #'ENGINE': 'django.db.backends.mysql',
+         #'NAME': 'tsdproject',
+         #'USER': 'root',
+         #'PASSWORD': 'root',
+         #'HOST': 'localhost',
+         #'PORT': '3306',
     }
 }
 
